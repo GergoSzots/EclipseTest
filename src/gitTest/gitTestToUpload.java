5df628734a47d5ef89f8fbb57608ef0b6cee0839
kartal <kartal@kartal-Latitude-3550>
2018-11-10 20:11:49 +0100

    First commit ever.

---
+++ src/gitTest/gitTestToUpload.java
@@ -0,0 +1,12 @@
+package gitTest;
+
+public class gitTestToUpload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello guys, I'm just a test atm.");
+		
+	}
+
+}
